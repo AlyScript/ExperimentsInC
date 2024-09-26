f4fc328c0370b926a58a3b1699619e27043372b3
AlyScript <adamaly93@gmail.com>
2024-09-26 21:27:48 +0100

    Error handling and being safe

--- threads/a.c
+++ threads/a.c
@@ -11,9 +11,22 @@ void* routine() {
 
 int main(int argc, char* argv[]) {
     pthread_t t1, t2; // contains information about the thread
-    pthread_create(&t1, NULL, &routine, NULL); // args: pointer to thread infro, thread attributes, pointer to function to run, args to function
-    pthread_create(&t2, NULL, &routine, NULL);
-    pthread_join(t1, NULL); // args: thread info, return value from thread -- this ensures that the calling thread waits for the new thread to finish
-    pthread_join(t2, NULL);
+    // args: pointer to thread infro, thread attributes, pointer to function to run, args to function
+    // the if statement checks if the thread was successfully created, since this could fail
+    if (pthread_create(&t1, NULL, &routine, NULL) != 0) {
+        return 1;
+    }
+    if (pthread_create(&t2, NULL, &routine, NULL) != 0) {
+        return 2;
+    }
+
+    // args: thread info, return value from thread -- this ensures that the calling thread waits for the new thread to finish
+    // the if statement checks if the thread was successfully joined, since this could fail
+    if (pthread_join(t1, NULL) != 0) {
+        return 3;
+    }
+    if (pthread_join(t2, NULL)) {
+        return 4;
+    }
     return 0;
 }
